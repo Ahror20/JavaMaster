0756865ef4ff6ff7f853aa5c22efce1b682f6f1c
ahror <ahrororzimurodov@icloud.com>
2023-11-25 17:36:51 +0500

    6 commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
     public static void main(String[] args) {
       Manager manager = new Manager();
       manager.addStudent("Ahror","Orzimurodov",20);
+      manager.addStudent("Alik","Joniyev",19);
+      manager.addStudent("Baxti","Shaxtiyev",23);
+
+
 
     }
 }
--- src/Manager.java
+++ src/Manager.java
@@ -6,6 +6,21 @@ public class Manager {
 
     public void addStudent(String name,String surname,Integer age){
         Student student = new Student();
+        student.setName(name);
+        student.setSurname(surname);
+        student.setAge(age);
         students.add(student);
+    }
+    public Student getStudent(String name){
+        int index=0;
+        for (Student student:students){
+            if (student!=null ) {
+                index++;
+                if (student.getName().equals(name)) {
+                    return students.get(index);
+                }
+            }
+        }
+        return null;
     }
 }
